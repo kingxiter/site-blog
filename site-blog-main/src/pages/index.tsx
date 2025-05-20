0ce7d2fea03aa53d72ef9029269c5b151c7a62a9
Igor <igor_fmellO@hotmail.com>
2025-05-20 16:23:19 -0300

    feat: criar seção Call to Action

--- site-blog-main/src/pages/index.tsx
+++ site-blog-main/src/pages/index.tsx
@@ -2,6 +2,7 @@ import { HeroSection } from "@/components/ui/hero-section";
 import { FeatureSection } from "@/components/ui/feature-section";
 import { SupportSection } from "@/components/ui/support-section";
 import { CustomerStorySection } from "@/components/ui/customer-story-section";
+import { CallToAction } from "@/components/ui/call-to-action";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <FeatureSection />
         <SupportSection/>
         <CustomerStorySection/>
+        <CallToAction/>
       </article>
     </>
   );
