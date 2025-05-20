72036fa156073e073d850fd89f0f235b31a08845
Igor <igor_fmellO@hotmail.com>
2025-05-20 11:29:04 -0300

    feat: criando conteúdo da seção Support

--- site-blog-main/src/pages/index.tsx
+++ site-blog-main/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { HeroSection } from "@/components/ui/hero-section";
 import { FeatureSection } from "@/components/ui/feature-section";
+import { SupportSection } from "@/components/ui/support-section";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <article className="flex flex-col">
         <HeroSection />
         <FeatureSection />
+        <SupportSection/>
       </article>
     </>
   );
