296b14b009fa3ffb55e14ff43f37fbef19e0268a
Igor <igor_fmellO@hotmail.com>
2025-05-27 19:36:10 -0300

    finalizando

--- site-blog-main/src/app/blog/[slug]/page.tsx
+++ site-blog-main/src/app/blog/[slug]/page.tsx
@@ -8,6 +8,14 @@ type BlogPostPage = {
     }>
 }
 
+export const revalidate = 60;
+
+export async function generateStaticParams() {
+  return allPosts.map((post) => ({
+    slug: post.slug
+  }));
+}
+
 export default async function BlogPostPage({params}: BlogPostPage) {
     const { slug } = await params; 
     const post = allPosts.find((post) => post.slug === slug);
