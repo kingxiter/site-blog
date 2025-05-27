948c6cedac761adeb4a8501a10178fd13889a547
Igor <igor_fmellO@hotmail.com>
2025-05-27 18:37:15 -0300

    criando error

---
+++ site-blog-main/src/app/component.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { useEffect } from "react";
+
+export const ErrorComponent = () => {
+  useEffect(() => {
+    throw new Error("Componente lançando um error");
+  }, []);
+
+  return <h2>ErrorComponent</h2>;
+};
--- site-blog-main/src/app/page.tsx
+++ site-blog-main/src/app/page.tsx
@@ -1,8 +1,5 @@
 import { LandingPage } from "@/templates/landing-page";
 
 export default function HomePage() {
-
-    
-    return <LandingPage/>;
-    
+  return <LandingPage />;
 }
