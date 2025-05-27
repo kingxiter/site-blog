f01b26963998fdfbebf96135ff614b352ddd7c20
Igor <igor_fmellO@hotmail.com>
2025-05-27 19:07:59 -0300

    Criando o component PostShare

--- site-blog-main/src/templates/blog/post-page.tsx
+++ site-blog-main/src/templates/blog/post-page.tsx
@@ -1,32 +1,25 @@
 import Image from "next/image";
 import Link from "next/link";
-import {Post} from ".contentlayer/generated";
+import { Post } from ".contentlayer/generated";
 
 import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    BreadcrumbList,
-    BreadcrumbSeparator,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 import { Avatar } from "@/components/avatar";
 import { Markdown } from "@/components/markdown";
-import { Button } from "@/components/ui/button";
-import { useShare } from "@/hooks";
+import { PostShare } from "./components/post-share";
 
 export type PostPageProps = {
-    post: Post;
-}
+  post: Post;
+};
 
-export const PostPage = ({ post }: PostPageProps ) => {
+export const PostPage = ({ post }: PostPageProps) => {
   const publishedDate = new Date(post?.date).toLocaleDateString("pt-BR");
   const postUrl = `https://site.set/blog/${post.slug}`;
-
-  const { shareButtons } = useShare({
-    url: postUrl,
-    title: post.title,
-    text: post.description,
-  });
 
   return (
     <main className="py-20 text-gray-100">
@@ -85,30 +78,11 @@ export const PostPage = ({ post }: PostPageProps ) => {
               <Markdown content={post.body.raw} />
             </div>
           </article>
-
-          <aside className="space-y-6">
-            <div className="rounder-lg bg-gray-700">
-              <h2 className="hidden md:block mb-4 text-heading-gx text-gray-100">
-                Compartilhar
-              </h2>
-
-              <div className="flex justify-between md:flex-col gap-2">
-                {shareButtons.map((provider) => (
-                  <Button
-                    key={provider.provider}
-                    onClick={() => provider.action()}
-                    variant="outline"
-                    className="w-fit md:w-full justify-start gap-2"
-                  >
-                    {provider.icon}
-                    <span className="hidden md:block">
-                        {provider.name}
-                    </span>
-                  </Button>
-                ))}
-              </div>
-            </div>
-          </aside>
+          <PostShare
+            url={postUrl}
+            title={post.title}
+            description={post.description}
+          />
         </div>
       </div>
     </main>
