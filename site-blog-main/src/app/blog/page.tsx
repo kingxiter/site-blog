b3b460e418bf0b912eb34f848d9c320c0e3eb51a
Igor <igor_fmellO@hotmail.com>
2025-05-27 23:49:25 -0300

    Melhorando o SEO na página de listagem Posts

--- site-blog-main/src/app/blog/page.tsx
+++ site-blog-main/src/app/blog/page.tsx
@@ -1,5 +1,28 @@
-import { BlogList } from "@/templates/blog"
+import type { Metadata } from "next";
 import { allPosts } from "contentlayer/generated"
+import { BlogList } from "@/templates/blog"
+
+export const metadata: Metadata = {
+  title: 'Blog',
+  description: 'Dicas e estrat´gias para impulsionar seu negócio',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Blog',
+    description: 'Dicas e estrat´gias para impulsionar seu negócio',
+    url: 'https://site-blog-six.vercel.app/og-image.jpg',
+    siteName: 'Site.Set',
+    locale: 'pt_BR',
+    type: 'website',
+    images: [
+      {
+        url: 'https://site-blog-six.vercel.app/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Blog',
+      },
+    ],
+  },
+};
 
 export default function BlogListPage () {
     const sortedPosts = allPosts.sort(
