530e5a34339d2c41362e3de4d804a23d55ff1b04
Igor <igor_fmellO@hotmail.com>
2025-05-28 00:11:07 -0300

    add seo in blog post page

--- site-blog-main/src/app/blog/[slug]/page.tsx
+++ site-blog-main/src/app/blog/[slug]/page.tsx
@@ -1,30 +1,49 @@
+import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { allPosts } from "contentlayer/generated";
 import { PostPage } from "@/templates/blog";
 
-type BlogPostPage = {
-    params: Promise<{
-        slug: string;
-    }>
+type BlogPostPageProps = {
+  params: Promise<{
+    slug: string;
+  }>;
+};
+
+export async function generatedMetadata({
+  params,
+}: BlogPostPageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const post = allPosts.find((post) => post.slug === slug);
+
+  if (!post) {
+    return {};
+  }
+  return {
+    title: post.title,
+    description: post.description,
+    authors: [{ name: post.author.name }],
+    robots: "index, follow",
+    openGraph: {
+      images: [post.image],
+    },
+  };
 }
 
 export const revalidate = 60;
 
 export async function generateStaticParams() {
   return allPosts.map((post) => ({
-    slug: post.slug
+    slug: post.slug,
   }));
 }
 
-export default async function BlogPostPage({params}: BlogPostPage) {
-    const { slug } = await params; 
-    const post = allPosts.find((post) => post.slug === slug);
+export default async function BlogPostPage({ params }: BlogPostPageProps) {
+  const { slug } = await params;
+  const post = allPosts.find((post) => post.slug === slug);
 
-    if (!post){
-        notFound();
-    }
+  if (!post) {
+    notFound();
+  }
 
-    return (
-        <PostPage post={post} />
-    )
-} 
+  return <PostPage post={post} />;
+}
